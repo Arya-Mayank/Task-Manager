be1a5931ba6655d86b93927195ada2cff93bfeff
Arya-Mayank <arya.mayank2001@gmail.com>
2022-02-16 20:15:20 +0530

    server code initial

--- app.js
+++ app.js
@@ -1,26 +1,32 @@
-
 const express = require('express');
 const app = express();
-const tasks = require('./routes/tasks');
-const connectDB = require('./db/connect');
-require('dotenv').config();
+const tasks = require('./routes/tasks')
 
-//middleware
-app.use(express.json());
+const connectDB = require('./db/connect')
+require('dotenv').config()
 
-//routes
+// middleware
+app.use(express.json())
+
+// routes
 app.get('/hello', (req, res) => {
-    res.send('Task Manager App');
-});
+    res.end("Hello")
+})
 
-app.use("/api/v1/tasks", tasks);
+app.use('/api/v1/tasks', tasks);
 
+
+// listen
 const port = 3000;
 
-const start = async() => {
+// wait for connection to the db to be established.
+const start = async ()=>{
     try {
-        await connectDB(process.env.MONGO_URI);
-        app.listen(port, console.log(`server started on port ${port} ...`));
+        await connectDB(process.env.MONGO_URI)
+        // if connection is established, then spin up the server
+        app.listen(port, ()=>{
+            console.log(`Server listening on port ${port}`)
+        })
     } catch (error) {
         console.log(error);
     }
